3636500dc70e8430dc8b36ce697d93117389884b
ArcticTech <115091265+TarekRehouli@users.noreply.github.com>
2024-07-21 00:28:29 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -49,14 +49,16 @@ const App = () => {
         <button style={{ padding: '2px 5px', cursor: 'pointer', backgroundColor: '#fff', border: '1px solid #ccc', borderRadius: '3px', fontSize: '10px' }} onClick={() => handleLanguageChange('fr')}>FR</button>
       </div>
       <Navbar language={language} />
-      <Hero language={language} />
+      <div id="home">
+          <Hero language={language} />
+        </div>
       <div className="container">
         <FixedIcon language={language} />
         <Title subtitle={translations[language].subtitle1} title={translations[language].title1} />
         <div id="courts">
           <Programs language={language} />
         </div>
-        <div id="home">
+        <div>
           <About language={language} />
         </div>
         <div>
