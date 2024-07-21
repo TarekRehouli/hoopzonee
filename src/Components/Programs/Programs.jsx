d75415423ae80c5558a0941d69f2003d251f5368
ArcticTech <115091265+TarekRehouli@users.noreply.github.com>
2024-07-20 22:52:43 -0400

    Update Programs.jsx

--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -5,30 +5,43 @@ import court_2 from "../../assets/court_2.jpg"
 import court_3 from "../../assets/court_3.jpg"
 import court_icon from "../../assets/court_icon.png"
 
-const Programs = () => {
+const translations = {
+  en: {
+    interior: 'Interior Court',
+    exterior: 'Exterior Court',
+    other: 'Other'
+  },
+  fr: {
+    interior: 'Court Intérieur',
+    exterior: 'Court Extérieur',
+    other: 'Autre'
+  }
+};
+
+const Programs = ({ language }) => {
   return (
     <div className='programs'>
-     <div className="program">
+      <div className="program">
         <img src={court_1} alt="" />
         <div className="caption">
-            <img src={court_icon} alt="" />
-            <p>Interior Court</p>
-            </div> 
+          <img src={court_icon} alt="" />
+          <p>{translations[language].interior}</p>
         </div> 
-        <div className="program">
+      </div> 
+      <div className="program">
         <img src={court_2} alt="" />
         <div className="caption">
-            <img src={court_icon} alt="" />
-            <p>Exterior Court</p>
+          <img src={court_icon} alt="" />
+          <p>{translations[language].exterior}</p>
         </div>
-        </div> 
-        <div className="program">
+      </div> 
+      <div className="program">
         <img src={court_3} alt="" />
         <div className="caption">
-            <img src={court_icon} alt="" />
-            <p>Other</p>
+          <img src={court_icon} alt="" />
+          <p>{translations[language].other}</p>
         </div>
-        </div> 
+      </div> 
     </div>
   )
 }
