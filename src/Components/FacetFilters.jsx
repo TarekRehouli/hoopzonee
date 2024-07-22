76534fd00731d70a757b00d32f836792dfe459c5
ArcticTech <115091265+TarekRehouli@users.noreply.github.com>
2024-07-21 22:22:01 -0400

    Create FacetFilters.jsx

---
+++ src/Components/FacetFilters.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const FacetFilters = ({ facets, selectedFacets, onFacetChange }) => {
+  const handleChange = (facet, value) => {
+    const newSelectedFacets = { ...selectedFacets };
+    if (newSelectedFacets[facet].includes(value)) {
+      newSelectedFacets[facet] = newSelectedFacets[facet].filter(item => item !== value);
+    } else {
+      newSelectedFacets[facet].push(value);
+    }
+    onFacetChange(newSelectedFacets);
+  };
+
+  return (
+    <div className="facets">
+      {Object.keys(facets).map(facet => (
+        <div key={facet} className="facet">
+          <h4>{facet}</h4>
+          {facets[facet].map(value => (
+            <label key={value}>
+              <input
+                type="checkbox"
+                checked={selectedFacets[facet].includes(value)}
+                onChange={() => handleChange(facet, value)}
+              />
+              {value}
+            </label>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FacetFilters;
